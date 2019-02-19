9e024d84424067c09a966fd33355da56d7acc117
matjukov-nikolaj <blatnoi.kolyan@yandex.ru>
2019-02-19 22:58:11 +0300

    add lw02

---
+++ lw02/src/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Frontend.Models;
+using System.Net.Http;
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload(string data)
+        {
+            string id = null; 
+            //TODO: send data in POST request to backend and read returned id value from response
+            if (data == null) {
+                return Ok("Empty request.");
+            }
+            string url = "http://127.0.0.1:5000/api/values";
+            HttpClient client = new HttpClient();
+            HttpContent content = new FormUrlEncodedContent(new[]{new KeyValuePair<string, string>("data", data)});
+            var response = client.PostAsync(url, content);
+            id = response.Result.Content.ReadAsStringAsync().Result;
+            return Ok(id);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
